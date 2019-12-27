064ce4a86f75c477756c6386f8866fcefae2ac3a
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-27 18:29:07 +0100

    ADDED: graphql middleware with dummy config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,59 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+    
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+  
+    type RootQuery {
+      events: [Event!]!
+    }
+    
+    type RootMutation {
+      createEvent(eventInput: EventInput): Event
+    }
+  
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return events
+    },
+    createEvent: (args) => {
+      const event = {
+        _id: Math.random().toString(),
+        ...args.eventInput
+      };
+      events.push(events);
+      return event;
+    }
+  },
+  graphiql: true
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
