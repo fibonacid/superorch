2c2a101a43e02649a0f1e20d046e13d127d415bd
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-27 21:56:20 +0100

    ADDED: createUser mutation

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Event = require('./models/events');
+const User = require('./models/users');
 
 const app = express();
 
@@ -20,11 +22,22 @@ app.use('/graphql', graphqlHttp({
       date: String!
     }
     
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+    }
+     
     input EventInput {
       title: String!
       description: String!
       price: Float!
       date: String!
+    }
+    
+    input UserInput {
+      email: String!
+      password: String!
     }
   
     type RootQuery {
@@ -33,6 +46,7 @@ app.use('/graphql', graphqlHttp({
     
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
   
     schema {
@@ -44,7 +58,7 @@ app.use('/graphql', graphqlHttp({
     events: () => {
       return Event.find().then(events => {
         return events.map(event => {
-          return { ...event._doc, _id: event.id }
+          return { ...event._doc }
         })
       }).catch(err => {
         console.log(err);
@@ -60,11 +74,35 @@ app.use('/graphql', graphqlHttp({
       });
       return event.save().then(result => {
         console.log(result);
-        return {...result._doc, _id: result.id }
+        return {...result._doc }
       }).catch(err => {
         console.log(err);
         return err;
       });
+    },
+    createUser: (args) => {
+      return User.findOne({email: args.userInput.email})
+        .then(user => {
+          if (user) {
+            throw new Error('User exists already')
+          }
+          return bcrypt.hash(args.userInput.password, 12)
+        })
+        .then(hashedPassword => {
+          const user = new User({
+            email: args.userInput.email,
+            password: hashedPassword
+          });
+          return user.save();
+        })
+        .then(result => {
+          console.log(result);
+          return {...result._doc, password: null }
+        })
+        .catch(err => {
+          console.log(err);
+          return err;
+        });
     }
   },
   graphiql: true
