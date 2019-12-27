d620fc66c46a8ce4cf450b7fb75b71b8cafb7671
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-27 17:39:27 +0100

    ADDED: use body-parser middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 app.listen(3000);
 console.log('Listening on port 3000');
