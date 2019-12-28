3eefa9ad62968c9f1166ded77989732799fc800a
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-28 20:19:05 +0100

    CHANGED: window loading entry point

--- public/electron.js
+++ public/electron.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -15,7 +17,10 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadURL(isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
 
   // Open the DevTools.
   win.webContents.openDevTools()
