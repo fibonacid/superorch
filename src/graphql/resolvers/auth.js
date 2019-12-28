042496bb755cc5f54ad52bfff231011c6e8d8a2c
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-28 18:28:00 +0100

    ADDED: login query

--- src/graphql/resolvers/auth.js
+++ src/graphql/resolvers/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../../models/users");
 
@@ -25,5 +26,28 @@ module.exports = {
       console.log(err);
       return err;
     }
+  },
+
+  login: async ({ email, password }) => {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("User doesn't exist");
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error("Password is incorrect");
+    }
+    const token = await jwt.sign(
+      { userId: user.id, email: user.email },
+      "somesupersecretkey",
+      {
+        expiresIn: "1h"
+      }
+    );
+    return {
+      userId: user.id,
+      token,
+      tokenExpiration: 1
+    };
   }
 };
