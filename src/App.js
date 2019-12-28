85ee81bbb3e8fbeed9bd89deb960957d1faf5fc8
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-29 00:22:04 +0100

    ADDED: routes and views (home and login)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
-
+import { HashRouter, Route } from "react-router-dom";
 const { ipcRenderer: ipc } = window.require("electron");
 
+//
+// Components
+//
+import Header from './components/Header';
+
+//
+// Views
+//
+import Home from "./views/home";
+import Login from "./views/login";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <HashRouter>
+      <div>
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
+      </div>
+    </HashRouter>
+  );
 }
 
 export default App;
