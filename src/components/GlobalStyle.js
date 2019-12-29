ed5d16cc05540ae56690b04530e916607b8edd5d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-29 18:46:35 +0100

    REMOVED: styled-normalize

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,10 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
 import reset from 'styled-reset'
-import normalize from "styled-normalize";
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
-  ${normalize}
   /* other styles */
   
   html {
