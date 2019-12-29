709207f5170eb8e0d3b859a3e6c1226b9d151893
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-29 18:43:04 +0100

    ADDED: responsive page template

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -6,6 +6,21 @@ const GlobalStyle = createGlobalStyle`
   ${reset}
   ${normalize}
   /* other styles */
+  
+  html {
+    height: 100%;
+    min-height: 100vh;
+  }
+  
+  body {
+    height: 100%;
+  }
+  
+  #root {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 export default GlobalStyle;
