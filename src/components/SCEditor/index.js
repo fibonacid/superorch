3d4832c94648560ad6f5aea2e4eb6bca65589590
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-30 21:58:44 +0100

    ADDED: SCEditor component

---
+++ src/components/SCEditor/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Editor, EditorState} from 'draft-js';
+
+// -----------------------------------
+// SuperCollider Editor
+// -----------------------------------
+
+class SCEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = editorState => this.setState({editorState});
+  }
+  render() {
+    return (
+      <Editor editorState={this.state.editorState} onChange={this.onChange} />
+    );
+  }
+}
+
+export default SCEditor;
