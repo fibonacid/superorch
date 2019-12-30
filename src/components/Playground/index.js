f1d4d5642d890c8050718617e2cf89c7fdeefa8a
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-30 21:58:58 +0100

    ADDED: title and text editor

--- src/components/Playground/index.js
+++ src/components/Playground/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import SCEditor from '../SCEditor'
+
+const StyledTitle = styled.h2`
+    padding: 10px 5px;
+    text-align: center; 
+    background: whitesmoke;
+    border-bottom: solid 1px lightgrey;
+`;
 
 function Playground(props) {
     return (
-        <div>Here is the playground</div>
+        <div>
+            <StyledTitle>SuperCollider Editor</StyledTitle>
+            <SCEditor />
+        </div>
     )
 }
 
