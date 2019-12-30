66113c57a73b7ed5275c359ca7f92e4cc0cf8e64
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-30 18:57:58 +0100

    ADDED: styles to FrameBox components

--- src/views/home.js
+++ src/views/home.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
+import authContext from "../context/auth-context";
 import Frame from "../components/Frame";
 import FrameBox from "../components/FrameBox";
+import styled from "styled-components";
+
+const StyledSidebar = styled(FrameBox)`
+  border-right: solid 1px lightgrey;
+`;
+
+const StyledMainbar = styled(FrameBox)``;
 
 function HomeView(props) {
+
+  const { token, userId } = useContext(authContext); 
+
   return (
     <Frame>
-      <FrameBox>Hello</FrameBox>
-      <FrameBox>Hello</FrameBox>
+      <StyledSidebar basis="200px">User List</StyledSidebar>
+      <StyledMainbar>Hello</StyledMainbar>
     </Frame>
   )
 }
