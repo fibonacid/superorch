03b0647ba2ea76506b977d1eacc73e0fdb6a8755
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2019-12-30 21:21:39 +0100

    ADDED: use localStorage to save and retrieve auth date

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,10 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+function todayPlusHours(h) {
+  const today = new Date();
+  today.setHours(today.getHours() + h);
+  return today;
+}
 
 function useAuth() {
+
     const [token, setToken] = useState(null);
     const [userId, setUserId] = useState(null);
     const [tokenExpiration, setTokenExpiration] = useState(null);
-  
+    
+    // Intialize auth variables with
+    // values from local storage.
+    useEffect(() => {
+      setToken(localStorage.getItem("token"));
+      setUserId(localStorage.getItem("userId"));
+      setTokenExpiration(localStorage.getItem("tokenExpiration"));
+    }, [])
+
     const login = (token, userId, tokenExpiration) => {
       setToken(token);
       setUserId(userId);
@@ -15,7 +30,19 @@ function useAuth() {
       setToken(null);
       setUserId(null);
       setTokenExpiration(null);
+      localStorage.clear();
     };
+
+    // When new auth data is retrieved:
+    useEffect(() => {
+      if (token && userId && tokenExpiration) {        
+        console.log('Logged in')
+        localStorage.setItem('token', token)
+        localStorage.setItem('userId', userId)
+        localStorage.setItem('tokenExpiration', tokenExpiration)
+      }
+    }, [token, userId, tokenExpiration]);
+
 
     return {token, userId, tokenExpiration, login, logout}
 }
