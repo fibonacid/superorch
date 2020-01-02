6110a1dace30035924edca19e82e16a6447250e4
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-02 17:48:02 +0100

    ADDED: display errors and loading

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import styled from 'styled-components';
 import User from './User';
 import useUsers from '../../hooks/useUsers';
 
+const StyledError = styled.span`
+    color: red;
+`;
+
 function UserList(props) {
 
-    const {users} = useUsers();
+    const { users, loading, errors } = useUsers();
 
-    return(
-        <ul>
-            {users.map((user, i) => <User key={i} user={user} />)}
-        </ul>
+    return (
+        <>
+        {loading && (
+            <span>Loading ...</span>
+        )}
+        {errors && (
+            Object.keys(errors).map(
+                (key, i) => (
+                    <StyledError key={i}>
+                        {errors[key].message}
+                    </StyledError>
+                )
+            )
+        )}
+        {users && (
+            <ul>
+                {users.map(
+                    (user, i) => <User key={i} user={user} />
+                )}
+            </ul>
+        )}
+        </>
     )
 }
 
