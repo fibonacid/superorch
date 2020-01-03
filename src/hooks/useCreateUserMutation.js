ac423871f1d187eba5e97c35a2741f56384e5295
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-03 22:39:41 +0100

    CHANGED: login once createUser mutation is completed

--- src/hooks/useCreateUserMutation.js
+++ src/hooks/useCreateUserMutation.js
@@ -1,17 +1,28 @@
 
+import { useContext } from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import { CREATE_USER_MUTATION } from '../data/api';
+import AuthContext from '../context/auth-context';
 
 export default function useCreateUserMutation() {
-   const [createUser, {data, loading, error}] = useMutation(
-      CREATE_USER_MUTATION, 
-      {
-        onCompleted: ({ createUser }) => {
-          console.log('Success', createUser._id);
-        },
-        onError: err => console.log(err.message) 
-      }
-    );
 
-    return [createUser, {data, loading, error}]
+  const context = useContext(AuthContext);
+
+  const [createUser, {data, loading, error}] = useMutation(
+    CREATE_USER_MUTATION, 
+    {
+      onCompleted: ({ createUser }) => {
+        console.log('Success', createUser.token);
+        // Save authentication data and leave.
+        context.login(
+          createUser.token,
+          createUser.userId,
+          createUser.tokenExpiration,
+        );
+      },
+      onError: err => console.log(err.message) 
+    }
+  );
+
+  return [createUser, {data, loading, error}]
 } 
