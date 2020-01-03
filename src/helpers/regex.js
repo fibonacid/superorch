d5361cc5463d9a19226d07b412a2fbf5429d0a22
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-03 18:54:46 +0100

    ADDED: password regex

--- src/helpers/regex.js
+++ src/helpers/regex.js
@@ -1,4 +1,4 @@
 export const EMAIL_REGEX = RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
 //Minimum 8 characters, at least one letter and one number:
-export const PASSWORD_REGEX = RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/i);
+export const PASSWORD_REGEX = RegExp(/^[a-zA-Z0-9!\-_@#$%^&*]{4,}$/i);
