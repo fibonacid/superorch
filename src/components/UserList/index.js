8146eac6770035e62adef8eeda42b57bb8527bca
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-05 22:59:36 +0100

    CHANGED: use styled-components macro

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from 'styled-components/';
 import User from './User';
 import useUsersQuery from '../../hooks/useUsersQuery';
 
