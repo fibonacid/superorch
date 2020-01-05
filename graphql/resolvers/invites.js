94ba8c3c6b86a46524700e4e67e975ddc624251c
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-05 19:50:25 +0100

    ADDED: save invite on sendInvite resolver

--- graphql/resolvers/invites.js
+++ graphql/resolvers/invites.js
@@ -1,6 +1,35 @@
+const Invite = require("../../models/invites");
+const User = require("../../models/users");
+const Orchestra = require("../../models/orchestras");
+
 module.exports = {
-  sendInvite: async (_, args) => {
-    throw new Error("Not implemented yet");
+  sendInvite: async (_, { orchestraId, email }, req) => {
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated");
+    }
+    try {
+      // Fetch orchestra to check that it actually exist
+      await Orchestra.findOne({
+        _id: orchestraId,
+        owner: req.userId
+      });
+
+      const user = await User.findOne({ email });
+
+      // Create a new invite
+      const invite = await Invite.create({
+        orchestraId,
+        email,
+        userId: user.id || null
+      });
+
+      const result = await invite.save();
+      // todo: use result id as email token
+      return result;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
   },
   acceptInvite: async (_, args) => {
     throw new Error("Not implemented yet");
