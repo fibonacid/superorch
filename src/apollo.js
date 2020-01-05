fdc57ab36f5277a2311258f96e884b85919cc3d2
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-05 12:32:34 +0100

    ADDED: error formatting link

--- src/apollo.js
+++ src/apollo.js
@@ -1,16 +1,29 @@
 import { ApolloClient } from "apollo-boost";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
-import { split } from 'apollo-link';
+import { ApolloLink, split } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
 import { WebSocketLink } from 'apollo-link-ws';
+import { onError } from "apollo-link-error";
 import { getMainDefinition } from 'apollo-utilities';
 
-const httpLink = new HttpLink({
-   credentials: 'same-origin',
-   uri: 'http://localhost:5000/graphql'
-});
+//
+// Formats GraphQL errors
+//
+const errorlink = onError(({ graphQLErrors, networkError }) => {
+   if (graphQLErrors)
+     graphQLErrors.map(({ message, locations, path }) =>
+       console.log(
+         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+       ),
+     );
+ 
+   if (networkError) console.log(`[Network error]: ${networkError}`);
+ });
 
+//
+// Sets Authorization header for every http request
+//
 const authLink = setContext((_, { headers }) => {
    // get the authentication token from local storage if it exists
    const token = localStorage.getItem('token');
@@ -23,9 +36,44 @@ const authLink = setContext((_, { headers }) => {
    }
  });
 
+ //
+ // Composes https links into a single entity
+ //
+const httpLink = ApolloLink.from(
+   [
+      authLink,
+      errorlink,
+      new HttpLink({
+         credentials: 'same-origin',
+         uri: 'http://localhost:5000/graphql'
+      })
+   ]
+);
+
+//
+// Create Websocket link
+//
 const wsLink = new WebSocketLink({
-   uri: 'ws://localhost:5000/graphql',
+   uri: 'ws://localhost:5000/graphql'
 });
+
+const subscriptionMiddleware = {
+   applyMiddleware: function(options, next) {
+      // Get the current context
+      const { graphqlContext } = options.getContext();
+
+      console.log({ graphqlContext });
+
+      // set it on the `options` which will be passed to the websocket.
+      // with Apollo Server it becomes: `ApolloServer({context: ({payload}) => (returns options)})
+      options.connectionParams = {
+         authToken: graphqlContext.token
+      }
+      next()
+   },
+};
+
+wsLink.subscriptionClient.use([subscriptionMiddleware]);
 
 // using the ability to split links, you can send data to each link
 // depending on what kind of operation is being sent
@@ -39,12 +87,13 @@ const link = split(
       );
    },
    wsLink,
-   authLink.concat(httpLink),
+   httpLink,
 );
 
 export default function configureClient() {
    return new ApolloClient({
       link, 
-      cache: new InMemoryCache(), 
+      cache: new InMemoryCache(),
+      fetchPolicy: 'network-only'
    });
 }
