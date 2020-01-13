820d02975032c75a456de28bd8f9da601b1f2a94
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-13 20:27:05 +0100

    ADDED: query singleOrchestra

--- client/src/components/UserList/index.js
+++ client/src/components/UserList/index.js
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components/";
 import ListItem from "./ListItem";
-import useUsersQuery from "../../hooks/useUsersQuery";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const QUERY = gql`
+  query($orchestraId: String!) {
+    singleOrchestra(orchestraId: $orchestraId) {
+      name
+      members {
+        _id
+        user {
+          _id
+        }
+      }
+    }
+  }
+`;
 
 //
 // Displays a list of users
 //
 export default function UserList() {
-  const { subscribeToUserJoined, data, loading, error } = useUsersQuery();
-
-  // Subscribe to new users
-  subscribeToUserJoined();
+  const { data, loading, error } = useQuery(QUERY, {
+    variables: { orchestraId: "5e1c9c834a14b807e8e746fd" }
+  });
 
   return (
     <>
       {loading && <span>Loading ...</span>}
       {error && <Error message={error.message} />}
-      {data && (
-        <ul>
-          {data.users.map((user, i) => (
-            <ListItem key={i} user={user} />
+      <ul>
+        {data &&
+          data.singleOrchestra.members.map((member, i) => (
+            <ListItem key={i} member={member} />
           ))}
-        </ul>
-      )}
+      </ul>
     </>
   );
 }
