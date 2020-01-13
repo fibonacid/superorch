eefbf901640b049671eb454806875ce005b80787
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-13 22:09:43 +0100

    ADDED: createOrchestraView component

---
+++ client/src/views/orchestras/create.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../partials/layout";
+
+function createOrchestraView(props) {
+  return <Layout>Create orchestra</Layout>;
+}
+
+export default createOrchestraView;
