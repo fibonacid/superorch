b62108ba309e21e440d5dbed97cde8eb073a0316
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-17 13:07:58 +0100

    ADDED: orchestra form

--- client/src/views/orchestras/edit.js
+++ client/src/views/orchestras/edit.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components/macro";
-import Sidebar from "../../components/Sidebar";
+import { useHistory, useParams } from "react-router-dom";
+import { useMutation } from "@apollo/react-hooks";
+import {
+  editOrchestraDocument,
+  orchestraListDocument
+} from "../../data/documents";
+import PrimaryLayout from "../../components/_layouts/PrimaryLayout";
+import PrimaryForm from "../../components/_forms/PrimaryForm";
+import OrchestraForm from "../../components/_forms/OrchestraForm";
 
-const StyledContainer = styled.div`
-  flex: 1;
-  display: flex;
+const StyledForm = styled(PrimaryForm)`
+  max-width: 300px;
+  border: solid 1px lightgrey;
+  border-radius: 10px;
+  margin: auto;
 `;
 
-function EditOrchestraView() {
+function EditOrchestraView(props) {
+  const history = useHistory();
+  const { orchestraId } = useParams();
+  const [editOrchestra, { data, loading, error }] = useMutation(
+    editOrchestraDocument
+  );
+
+  function authenticate(values) {
+    editOrchestra({
+      variables: { orchestraId, name: values.name },
+      refetchQueries: [{ query: orchestraListDocument }]
+    });
+  }
+
+  useEffect(() => {
+    if (data && props.onSuccess) {
+      history.push("/");
+    }
+  }, [data]);
+
   return (
-    <StyledContainer>
-      <Sidebar />
-      <div>Edit Orchestra</div>
-    </StyledContainer>
+    <PrimaryLayout>
+      <StyledForm title="Edit Orchestra">
+        <OrchestraForm authenticate={authenticate} />
+      </StyledForm>
+      {loading && <span>Loading ...</span>}
+      {error && <span>{error.message}</span>}
+    </PrimaryLayout>
   );
 }
 
