881966dbfb28acc94c0a73203e46e8ce6bfb4917
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-18 12:06:21 +0100

    ADDED: key param to playground component

--- client/src/views/orchestras/:id/index.js
+++ client/src/views/orchestras/:id/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import Playground from "../../../components/Playground";
 import PrimaryLayout from "../../../components/_layouts/PrimaryLayout";
 
 function OrchestraIndexView() {
+  const params = useParams();
+
   return (
     <PrimaryLayout>
-      <Playground />
+      <Playground key={params.id} />
     </PrimaryLayout>
   );
 }
