b62dd8b3ec5966ea25e83e7dede09e95841563fa
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-18 16:13:57 +0100

    ADDED: display number of members

--- client/src/components/Sidebar/MemberList/index.js
+++ client/src/components/Sidebar/MemberList/index.js
@@ -4,16 +4,24 @@ import ListItem from "./ListItem";
 
 const StyledContainer = styled.ul`
   margin: 30px 0 5px 0;
+  font-size: 14px;
 `;
 
 //
 // Displays a list of users
 //
 function MemberList({ members }) {
+  const userId = localStorage.getItem("userId");
+
+  // Filter out the current user
+  const filtered = members.filter(member => member.user._id !== userId);
+
   return (
     <StyledContainer>
-      {members &&
-        members.map((member, i) => <ListItem key={i} member={member} />)}
+      <p>{`${filtered.length} Members`}</p>
+      {filtered.map((m, i) => (
+        <ListItem key={i} member={m} />
+      ))}
     </StyledContainer>
   );
 }
