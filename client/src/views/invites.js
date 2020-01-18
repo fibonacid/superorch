cca3f185680836d4fbed02bcf93d913aa58d62dd
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-18 18:34:26 +0100

    CHANGED: styles

--- client/src/views/invites.js
+++ client/src/views/invites.js
@@ -1,15 +1,29 @@
 import React from "react";
-import PrimaryOverlay from "../components/_layouts/PrimaryOverlay";
+import FullScreenOverlay from "../components/_overlays/FullScreenOverlay";
 import styled from "styled-components/macro";
 
 const StyledContainer = styled.div`
   color: white;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
 `;
 
 export default function InvitesView() {
   return (
-    <PrimaryOverlay>
-      <StyledContainer>Here are my Invites</StyledContainer>
-    </PrimaryOverlay>
+    <FullScreenOverlay>
+      <StyledContainer>
+        <h1>My Invites</h1>
+        <ol>
+          <li>bla</li>
+          <li>bla</li>
+          <li>bla</li>
+          <li>bla</li>
+          <li>bla</li>
+        </ol>
+      </StyledContainer>
+    </FullScreenOverlay>
   );
 }
