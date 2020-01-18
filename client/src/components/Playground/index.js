0498d292e63d00cf11bd73f9c1a3a7bccc639b56
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-18 12:22:50 +0100

    ADDED: console and code editor components

--- client/src/components/Playground/index.js
+++ client/src/components/Playground/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
-import CodeEditor from "./CodeEditor";
+import CodeEditor from "../CodeEditor";
 
 const StyledContainer = styled.div`
   flex: 1;
--- client/src/views/orchestras/:id/index.js
+++ client/src/views/orchestras/:id/index.js
@@ -1,14 +1,29 @@
 import React from "react";
+import styled from "styled-components/macro";
 import { useParams } from "react-router-dom";
-import Playground from "../../../components/Playground";
 import PrimaryLayout from "../../../components/_layouts/PrimaryLayout";
+import CodeEditor from "../../../components/CodeEditor";
+import Console from "../../../components/Console";
+
+const StyledContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledConsole = styled(Console)`
+  flex: 0 1 30%;
+`;
 
 function OrchestraIndexView() {
   const params = useParams();
 
   return (
     <PrimaryLayout>
-      <Playground key={params.id} />
+      <StyledContainer>
+        <CodeEditor key={params.id} />
+        <StyledConsole />
+      </StyledContainer>
     </PrimaryLayout>
   );
 }
