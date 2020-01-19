31f20fe05f29b87e363c26a0d8f8358bbf1c192a
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-19 21:12:32 +0100

    ADDED: pass authentication function to form

--- client/src/views/orchestras/:id/invites.js
+++ client/src/views/orchestras/:id/invites.js
@@ -1,25 +1,55 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useCallback } from "react";
 import styled from "styled-components/macro";
+import { useParams } from "react-router-dom";
+import { useMutation } from "@apollo/react-hooks";
+import { sendInviteDocument } from "../../../data/documents";
 import PrimaryLayout from "../../../components/_layouts/PrimaryLayout";
 import PrimaryForm from "../../../components/_forms/PrimaryForm";
 import InviteForm from "../../../components/_forms/InviteForm";
 
-const StyledForm = styled(PrimaryForm)`
+const StyledContainer = styled.div`
   max-width: 300px;
+  margin: auto;
+`;
+
+const StyledForm = styled(PrimaryForm)`
   border: solid 1px lightgrey;
   border-radius: 10px;
-  margin: auto;
+`;
+
+const StyledStatus = styled.div`
+  text-align: center;
+  margin-top: 15px;
 `;
 
 function InvitesOrchestraView() {
   const params = useParams();
 
+  const [sendInvite, { loading, data, error }] = useMutation(
+    sendInviteDocument
+  );
+
+  const authenticate = useCallback(values => {
+    sendInvite({
+      variables: {
+        orchestraId: params.id,
+        email: values.email
+      }
+    });
+  }, []);
+
   return (
     <PrimaryLayout back={true} rootpath={"/orchestras/" + params.id}>
-      <StyledForm title="Send Invite">
-        <InviteForm />
-      </StyledForm>
+      <StyledContainer>
+        <StyledForm title="Send Invite">
+          <InviteForm authenticate={authenticate} />
+        </StyledForm>
+        <StyledStatus>
+          {loading && <p>loading ...</p>}
+          {data && <p>success</p>}
+          {error && <p>{error.message}</p>}
+        </StyledStatus>
+      </StyledContainer>
     </PrimaryLayout>
   );
 }
