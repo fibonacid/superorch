33c55e90e87d265fa494ff9b3be363435f768d51
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-20 17:42:12 +0100

    ADDED: color the icon red when there is at least one notification

--- client/src/components/Navigation/Inbox/index.js
+++ client/src/components/Navigation/Inbox/index.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { notificationsDocument } from "../../../data/documents";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
 import Widget from "../../_ui/Widget";
@@ -6,16 +8,33 @@ import Notifications from "../../Notifications";
 
 function Inbox(props) {
   const [visible, setVisible] = useState(false);
+  const [total, setTotal] = useState(0);
+  const { data } = useQuery(notificationsDocument);
 
   const toggle = () => {
     setVisible(!visible);
   };
+
+  // Count notifications
+  useEffect(
+    function() {
+      if (data) {
+        let count = 0;
+        for (const key in data) {
+          count += data[key].length;
+        }
+        setTotal(count);
+      }
+    },
+    [data]
+  );
 
   return (
     <div>
       <FontAwesomeIcon
         onClick={toggle}
         icon={faBell}
+        color={total > 0 ? "red" : null}
         style={{ cursor: "pointer" }}
       />
       {visible && (
