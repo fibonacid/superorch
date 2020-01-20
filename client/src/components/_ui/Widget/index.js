8d5d599c929e61fbfb6eab011dbc40cd7cb7872f
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-20 22:06:28 +0100

    ADDED: visible property

--- client/src/components/_ui/Widget/index.js
+++ client/src/components/_ui/Widget/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components/macro";
+import styled, { css } from "styled-components/macro";
 import Draggable from "react-draggable";
 
 const StyledContainer = styled.div`
@@ -10,12 +10,21 @@ const StyledContainer = styled.div`
   min-width: 200px;
   background: whitesmoke;
   color: black;
-  opacity: 0.97;
   border: solid 1px lightgrey;
   border-radius: 10px;
   padding: 10px;
   font-size: 14px;
   cursor: grab;
+  ${props =>
+    props.visible
+      ? css`
+          opacity: 0.95;
+          pointer-events: auto;
+        `
+      : css`
+          opacity: 0;
+          pointer-events: none;
+        `}
 `;
 
 const StyledHeader = styled.div`
@@ -33,7 +42,7 @@ const StyledClose = styled.span`
 function Widget(props) {
   return (
     <Draggable>
-      <StyledContainer>
+      <StyledContainer visible={props.visible}>
         <StyledHeader>
           <StyledClose onClick={props.close}>close</StyledClose>
         </StyledHeader>
