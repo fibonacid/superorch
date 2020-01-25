e4f9f23555ef36a246f0243d30b4a52f63466bd7
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-25 15:00:38 +0100

    ADDED: user profile form

--- client/src/views/account.js
+++ client/src/views/account.js
@@ -1,6 +1,22 @@
 import React from "react";
 import styled from "styled-components/macro";
+import SecondaryLayout from "../components/_layouts/SecondaryLayout";
+import PrimaryForm from "../components/_forms/PrimaryForm";
+import UserProfileForm from "../components/_forms/UserProfileForm";
+
+const StyledForm = styled(PrimaryForm)`
+  max-width: 300px;
+  border: solid 1px lightgrey;
+  border-radius: 10px;
+  margin: auto;
+`;
 
 export default function AccountView() {
-  return <div>Account Settings</div>;
+  return (
+    <SecondaryLayout>
+      <StyledForm title="User Settings">
+        <UserProfileForm />
+      </StyledForm>
+    </SecondaryLayout>
+  );
 }
