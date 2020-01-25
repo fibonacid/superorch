2760bdaad4ce696fd2d77d9c0b88c409d4c42bfb
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-25 20:19:43 +0100

    FIXED: wrong return type on launchSuperCollider function

--- client/public/lib/supercollider.js
+++ client/public/lib/supercollider.js
@@ -2,7 +2,7 @@ const sc = require("supercolliderjs");
 
 const options = {
   // post verbose messages to console
-  debug: true,
+  //debug: true,
   // echo all commands sent TO sclang to console
   echo: true
 };
@@ -11,7 +11,8 @@ async function launchSuperCollider() {
   // Boot supercollider interpreter
   const sclang = await sc.lang.boot(options);
   // Boot supercollider sound server
-  return sclang.interpret(`s = Server.default; s.boot;`);
+  await sclang.interpret(`s = Server.default; s.boot;`);
+  return sclang;
 }
 
 module.exports = {
