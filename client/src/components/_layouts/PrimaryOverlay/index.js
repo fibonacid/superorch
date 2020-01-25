3bcaef7b148e6c877c88e55c2001ce9febbef2b9
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-25 12:55:17 +0100

    ADDED: flex column display

--- client/src/components/_layouts/PrimaryOverlay/index.js
+++ client/src/components/_layouts/PrimaryOverlay/index.js
@@ -8,6 +8,10 @@ const StyledContainer = styled.div`
   bottom: 0;
   right: 0;
   background: rgba(0, 0, 0, 0.7);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 function PrimaryOverlay({ children }) {
