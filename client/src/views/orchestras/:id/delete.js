bad1b964d7114e59c98f2713a382e1f58319a87f
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-25 12:55:35 +0100

    ADDED: container div

--- client/src/views/orchestras/:id/delete.js
+++ client/src/views/orchestras/:id/delete.js
@@ -1,9 +1,20 @@
 import React from "react";
-//import styled from "styled-components";
+import styled from "styled-components/macro";
+import { useMutation } from "@apollo/react-hooks";
+import { deleteOrchestraDocument } from "../../../data/documents";
 import PrimaryOverlay from "../../../components/_layouts/PrimaryOverlay";
 
+const StyledContainer = styled.div`
+  background: white;
+  padding: 10px;
+`;
+
 function DeleteOrchestraView() {
-  return <PrimaryOverlay>Are you sure ?</PrimaryOverlay>;
+  return (
+    <PrimaryOverlay>
+      <StyledContainer>Are you sure ?</StyledContainer>
+    </PrimaryOverlay>
+  );
 }
 
 export default DeleteOrchestraView;
