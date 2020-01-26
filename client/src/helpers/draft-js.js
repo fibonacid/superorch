9ce0b3a501f96bb240bdb0c8af9179483729a07d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-26 22:50:11 +0100

    ADDED: findEntities function

---
+++ client/src/helpers/draft-js.js
@@ -0,0 +1,9 @@
+function findEntities(entityType, contentBlock, callback, contentState) {
+  contentBlock.findEntityRanges(character => {
+    const entityKey = character.getEntity();
+    return (
+      entityKey !== null &&
+      contentState.getEntity(entityKey).getType() === entityType
+    );
+  }, callback);
+}
