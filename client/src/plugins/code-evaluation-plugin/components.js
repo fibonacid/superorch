c363eff068f6ee8666767f4df172e981ddf234f4
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-27 11:47:58 +0100

    ADDED: blink animation

--- client/src/plugins/code-evaluation-plugin/components.js
+++ client/src/plugins/code-evaluation-plugin/components.js
@@ -1,17 +1,23 @@
 import React from "react";
+import styled, { keyframes } from "styled-components/macro";
+
+const blink = keyframes`
+  0% {
+    background: rgba(0,0,0);
+  }
+  100% {
+    background: rgba(0,255,0, 1.0);
+  }
+`;
+
+const StyledSpan = styled.span`
+  animation: 0.3s ${blink};
+`;
 
 export function EvaluatedSpan({ entityKey, contentState, children }) {
   const entity = contentState.getEntity(entityKey);
 
-  console.log({ data: entity.data });
+  console.log(entity.data);
 
-  return (
-    <span
-      style={{
-        background: "yellow"
-      }}
-    >
-      {children}
-    </span>
-  );
+  return <StyledSpan>{children}</StyledSpan>;
 }
