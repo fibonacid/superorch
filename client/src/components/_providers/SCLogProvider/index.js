4549381c022187170eaa29490116424c5d9e8fdd
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-29 23:33:35 +0100

    CHANGED: remove initial lines

--- client/src/components/_providers/SCLogProvider/index.js
+++ client/src/components/_providers/SCLogProvider/index.js
@@ -1,13 +1,8 @@
 import React, { useState, useMemo } from "react";
 import SCLogContext from "../../../context/sclog-context";
 
-const initialState = [
-  { type: "stdin", value: "2 + 2" },
-  { type: "stdout", value: "4" }
-];
-
 export default function SCLogProvider(props) {
-  const [lines, setLines] = useState(initialState);
+  const [lines, setLines] = useState([]);
 
   const pushLine = line => {
     setLines([...lines, line]);
