7fe1c6a8a4455f09360e657843c394fd21ccbfdf
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-31 12:15:37 +0100

    REMOVED: PrimaryOverlay

--- src/views/orchestras/:id/delete.js
+++ src/views/orchestras/:id/delete.js
@@ -3,7 +3,6 @@ import styled from "styled-components/macro";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { orchestraDocument } from "../../../config/documents";
-import PrimaryOverlay from "../../../components/_layouts/PrimaryOverlay";
 import DeleteOrchestraForm from "../../../components/_forms/DeleteOrchestraForm";
 import PrimaryForm from "../../../components/_miscellaneous/PrimaryForm";
 
@@ -19,18 +18,16 @@ function DeleteOrchestraView() {
   });
 
   return (
-    <PrimaryOverlay>
-      <StyledContainer>
-        {data && (
-          <PrimaryForm title="Delete Orchestra">
-            <p>
-              Confirm by entering the name of the orchestra you want to delete
-            </p>
-            <DeleteOrchestraForm orchestra={data.orchestraById} />
-          </PrimaryForm>
-        )}
-      </StyledContainer>
-    </PrimaryOverlay>
+    <StyledContainer>
+      {data && (
+        <PrimaryForm title="Delete Orchestra">
+          <p>
+            Confirm by entering the name of the orchestra you want to delete
+          </p>
+          <DeleteOrchestraForm orchestra={data.orchestraById} />
+        </PrimaryForm>
+      )}
+    </StyledContainer>
   );
 }
 
