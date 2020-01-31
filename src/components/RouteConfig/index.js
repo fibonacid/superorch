9c86e956a4c5e5da140f8b9ade1212b7a2c04b86
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-31 14:37:03 +0100

    ADDED: log pathname to console

--- src/components/RouteConfig/index.js
+++ src/components/RouteConfig/index.js
@@ -1,9 +1,15 @@
-import React from "react";
-import { Switch } from "react-router-dom";
+import React, {useEffect} from "react";
+import { Switch, useLocation } from "react-router-dom";
 import routes from "../../config/routes";
 import RouteWithSubRoutes from '../_miscellaneous/RouteWithSubRoutes';
 
 export default function RouteConfig() {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location.pathname)
+  }, [location.pathname]);
+
   return (
     <Switch>
       {routes.map((route, i) => (
