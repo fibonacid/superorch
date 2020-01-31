dce7044b3ef4f4d3ddf5743161a0c5c89c54cb68
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-31 16:12:50 +0100

    ADDED: AccountNav component

---
+++ src/components/AccountNav/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+import { Link } from 'react-router-dom'
+
+const StyledLink = styled(Link)`
+   color: black;
+   text-decoration: none;
+   display: block;
+   padding: 10px;
+   border-bottom: solid 1px lightgrey;
+`;
+
+export default function AccountNav() {
+   return (
+      <ul>
+         <li>
+            <StyledLink to={"/account/profile"}>User profile</StyledLink>
+         </li>
+      </ul>
+   )
+}
