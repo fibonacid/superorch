c9ea1ba0149335f77e48cc1bda0f2cf2cee6633d
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-31 15:31:22 +0100

    ADDED: modals

--- src/views/orchestras/show.js
+++ src/views/orchestras/show.js
@@ -1,14 +1,34 @@
 import React from "react";
-import {Switch} from 'react-router-dom';
-import RouteWithSubRoutes from '../../components/_miscellaneous/RouteWithSubRoutes';
+import styled from "styled-components/macro";
+import { Switch, Route, useLocation } from "react-router-dom";
+import Modal from "../../components/_miscellaneous/Modal";
+
+const StyledContainer = styled.div`
+  flex: 1;
+  display: flex;
+`;
 
 function OrchestraShowView({ routes }) {
+  const location = useLocation();
+  let background = location.state && location.state.background;
+
   return (
-    <Switch>
-      {routes.map((route, i) => (
-        <RouteWithSubRoutes key={i} {...route} />
-      ))}
-    </Switch>
+    <StyledContainer>
+      <Switch location={background || location}>
+        {routes.map((route, i) => (
+          <Route key={i} {...route} />
+        ))}
+      </Switch>
+      {background && (
+        <Switch>
+          {routes.map((route, i) => (
+            <Route key={i} {...route}>
+              <Modal children={<route.component/>}/>
+            </Route>
+          ))}
+        </Switch>
+      )}
+    </StyledContainer>
   );
 }
 
