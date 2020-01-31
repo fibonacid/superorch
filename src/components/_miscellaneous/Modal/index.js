84caf3630edf17e0c2785512df94bc07403221bc
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-01-31 12:17:23 +0100

    ADDED: Modal component

---
+++ src/components/_miscellaneous/Modal/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const StyledWrapper = styled.div`
+   position: absolute;
+   top: 0;
+   left: 0;
+   bottom: 0;
+   right: 0;
+   background: rgba(0, 0, 0, 0.25);
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+`;
+
+export default function Modal(props) {
+   return (
+      <StyledWrapper>
+         {props.children}
+      </StyledWrapper>
+   )
+}
