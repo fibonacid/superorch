e6e415637169172d2cc131d8a3f3cb9647d28003
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-02 16:39:10 +0100

    ADDED: sendMessageToMember resolver

--- graphql/resolvers/messages.js
+++ graphql/resolvers/messages.js
@@ -2,7 +2,7 @@ const { PubSub } = require("apollo-server-express");
 const Channel = require("../../models/channel");
 const Message = require("../../models/message");
 const Member = require("../../models/members");
-const { transformChannelMessage } = require("./_transforms");
+const { transformChannelMessage, transformPrivateMessage } = require("./_transforms");
 
 const NEW_PRIVATE_MESSAGE = "NEW_PRIVATE_MESSAGE";
 const NEW_CHANNEL_MESSAGE = "NEW_CHANNEL_MESSAGE";
@@ -12,10 +12,41 @@ const pubsub = new PubSub();
 exports.Mutation = {
   sendMessageToMember: async (
      _,
-    messageInput,
+    { messageInput },
     { isAuth, userId, loaders }
   ) => {
-    return new Error("Not implemented yet");
+    try {
+      if (!isAuth) {
+        throw new Error("Unauthenticated");
+      }
+      
+      // Check if member exists
+      const member = await Member.findOne({ user: userId });
+      if (!member) {
+          return new Error("Member doesn't exist")
+      }
+
+      // Check if receiveing member exists
+      const receiver = await Member.findById(messageInput.memberId);
+      if (!receiver) {
+          return new Error("Receiveng member doesn't exist");
+      }
+
+      // Create message
+      const message = await Message.create({
+          member: receiver,
+          from: member,
+          orchestra: member.orchestra,
+          context: messageInput.context,
+          format: messageInput.format,
+          value: messageInput.value,
+          body: messageInput.body
+      })
+      
+      return transformPrivateMessage(message.id, loaders);
+    } catch(err) {
+      return err;
+    }
   },
 
   sendMessageToChannel: async (
