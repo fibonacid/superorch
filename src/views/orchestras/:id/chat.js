5d811cfda955064fa85994ad7bf835f0e24b31de
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-03 17:31:45 +0100

    ADDED: view for orchestra chat

---
+++ src/views/orchestras/:id/chat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function OrchestraChatView() {
+   return (
+      <ChannelChatView />
+   )
+}
+
+function PrivateChatView() {
+   return <div>this is a private chat</div>
+}
+
+function ChannelChatView() {
+   return <div>this is a private chat</div>
+}
