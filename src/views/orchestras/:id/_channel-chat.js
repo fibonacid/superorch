60fc412c6890d4ad273143c928fdfb4cca61951b
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-03 23:42:41 +0100

    ADDED: use channelById query

--- src/views/orchestras/:id/_channel-chat.js
+++ src/views/orchestras/:id/_channel-chat.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { channelByIdDocument } from '../../../config/documents';
+import { useParams } from 'react-router-dom';
 
 export default function ChannelChatView({ channelId }) {
-   return <div>this is a chat on channel {channelId}</div>
+   const params = useParams();
+   const orchestraId = params.id;
+
+   const { data, loading, error } = useQuery(channelByIdDocument, {
+      variables: {
+         orchestraId,
+         channelId
+      }
+   });
+
+   return (
+      <div>
+         {data && <h2> {data.channelById.name}</h2>}
+         {error && <span>{error.message}</span>}
+         {loading && <span>loading ...</span>}
+      </div>
+   )
 }
