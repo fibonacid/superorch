4ebcb92f95be66b99a7bd7b1b624867987d03911
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-03 22:41:45 +0100

    ADDED: channelById resolver

---
+++ graphql/resolvers/channels.js
@@ -0,0 +1,39 @@
+const Member = require("../../models/members");
+const Channel = require('../../models/channel');
+const { transformChannel } = require('./_transforms');
+
+exports.Query = {
+  channelById: async (
+    _,
+    { orchestraId, channelId },
+    { isAuth, userId, loaders }
+  ) => {
+    try {
+      if (!isAuth) {
+        throw new Error("Unauthenticated");
+      }
+
+      const member = await Member.findOne({
+         orchestra: orchestraId,
+         user: userId
+      });
+      if (!member) {
+         throw new Error("Member doesn't exist");
+      }
+
+      const channel = await Channel.findOne({
+         _id: channelId,
+         members: {
+            $in: [member]
+         }
+      });
+      if (!channel) {
+         throw new Error("Channel doesn't exist");
+      }
+
+      return transformChannel(channel.id, loaders);
+    } catch (err) {
+      return err;
+    }
+  }
+};
