2c994aa7eee0232fd5474507832e27f8dfa9c88c
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 13:55:13 +0100

    ADDED: fetch private messages with specified member

--- src/views/orchestras/:id/_private-chat.js
+++ src/views/orchestras/:id/_private-chat.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-// import { useQuery } from "@apollo/react-hooks";
+import { useQuery } from "@apollo/react-hooks";
 import useMember from '../../../hooks/useMember';
-// import { memberByIdDocument } from "../../../config/documents";
+import { privateMessagesDocument } from "../../../config/documents";
 
 export default function PrivateChatView({ memberId }) {
   const params = useParams();
@@ -10,12 +10,27 @@ export default function PrivateChatView({ memberId }) {
 
   const member = useMember(orchestraId, memberId);
 
+  const {data, loading, error} = useQuery(privateMessagesDocument, {
+    variables: {
+      orchestraId,
+      memberId
+    }
+  });
+
   return (
     <div>
       <span>this is a private chat</span>
       {member && <span> with {member.user.name}</span>}
-      {/* {error && <span>{error.message}</span>}
-      {loading && <span>loading ...</span>} */}
+      {data && (
+         data.privateMessages.map((message, index) => (
+            <div key={index}>
+               <span>{message.from.user.name} sent: </span>
+               <span>{message.body}</span>
+            </div>
+         ))
+      )}
+      {error && <span>{error.message}</span>}
+      {loading && <span>loading ...</span>}
     </div>
   );
 }
