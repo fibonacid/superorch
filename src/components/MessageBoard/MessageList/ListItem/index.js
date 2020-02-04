3af24cfc1e0acc68e7f663f5a2776ed19f9208d4
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 18:46:42 +0100

    ADDED: show messages sent by user on the right

--- src/components/MessageBoard/MessageList/ListItem/index.js
+++ src/components/MessageBoard/MessageList/ListItem/index.js
@@ -1,26 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import useMember from "../../../../hooks/useMember";
 
 const StyledContainer = styled.li`
   margin: 5px;
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: ${props => (props.right ? "row-reverse" : "row")};
 `;
 
 const StyledInner = styled.div`
-   display: inline;
-   background: white;
-   padding: 8px;
-   border-radius: 8px;
-   font-size: 14px;
+  display: inline;
+  background: white;
+  padding: 8px;
+  border-radius: 8px;
+  font-size: 14px;
 `;
 
 export default function ListItem({ message }) {
+  const from = message.from.user._id;
+  const isLoggedUser = from === localStorage.getItem("userId");
+
   return (
-    <StyledContainer>
-       <StyledInner>
-         {message.body}
-       </StyledInner>
+    <StyledContainer right={isLoggedUser}>
+      <StyledInner>{message.body}</StyledInner>
     </StyledContainer>
   );
 }
