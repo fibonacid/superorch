b17c3c59a110c7731c183a2c084f25443e5510c7
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 23:31:34 +0100

    ADDED: page header

--- src/views/orchestras/show/members/index.js
+++ src/views/orchestras/show/members/index.js
@@ -1,14 +1,30 @@
 import React from "react";
-import {Switch} from 'react-router-dom';
+import {Switch, useParams} from 'react-router-dom';
+import useMember from "../../../../hooks/useMember";
+import * as chatLayout from "../../../../components/_layouts/chatLayout";
 import RouteWithSubRoutes from '../../../../components/_miscellaneous/RouteWithSubRoutes';
 
 function OrchestraMemberIndexView({ routes }) {
+  const { 
+    orchestra: orchestraId,
+    member: memberId 
+  } = useParams();
+
+  const member = useMember(orchestraId, memberId);
+
+  // if(!member) {
+  //   return <div>... loading</div>
+  // }
+
   return (
-    <Switch>
-      {routes.map((route, i) => (
-        <RouteWithSubRoutes key={i} {...route} />
-      ))}
-    </Switch>
+    <chatLayout.Container>
+      <chatLayout.Header>member</chatLayout.Header>
+      <Switch>
+        {routes.map((route, i) => (
+          <RouteWithSubRoutes key={i} {...route} />
+        ))}
+      </Switch>
+    </chatLayout.Container>
   );
 }
 
