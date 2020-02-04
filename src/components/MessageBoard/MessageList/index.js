81d831174722c9b8064cd2b4a4ab6e1fffee71d3
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 15:25:02 +0100

    CHANGED: styles

--- src/components/MessageBoard/MessageList/index.js
+++ src/components/MessageBoard/MessageList/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React from "react";
 import styled from 'styled-components/macro';
+import ListItem from "./ListItem";
 
 const StyledContainer = styled.div`
    flex: 1;
+   margin: 10px 5px;
+`;
+
+const StyledList = styled.ul`
+  display: flex;
+  flex-direction: column;
 `;
 
 export default function MessageList({ messages }) {
-   return (
-      <StyledContainer>
-         {messages.map((message, index) => (
-            <div key={index}>
-               <span>{message.from.user.name} sent: </span>
-               <span>{message.body}</span>
-            </div>
-         ))}
-      </StyledContainer>
-   )
+  return (
+    <StyledContainer>
+      <StyledList>
+        {messages.map((message, index) => (
+          <ListItem key={index} message={message} />
+        ))}
+      </StyledList>
+    </StyledContainer>
+  );
 }
