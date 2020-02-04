ace76f36e4a7b2787cb39582854f39b677f19ba4
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 15:24:29 +0100

    ADDED: title and styles

--- src/components/MessageBoard/index.js
+++ src/components/MessageBoard/index.js
@@ -7,11 +7,20 @@ const StyledContainer = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
+  background: rgb(231, 221, 212);
 `;
 
-export default function MessageBoard({ messages, onSend }) {
+const StyledTitle = styled.h2`
+   text-align: center;
+   padding: 5px 0;
+   background: whitesmoke;
+   border-bottom: solid 1px lightgrey;
+`;
+
+export default function MessageBoard({ title, messages, onSend }) {
   return (
     <StyledContainer>
+      <StyledTitle>{title}</StyledTitle>
       <MessageList messages={messages} />
       <InputBox onSend={onSend} />
     </StyledContainer>
