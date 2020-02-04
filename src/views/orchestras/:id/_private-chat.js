d25199ba477e7e1e883e7e09d5a7353669b95e87
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 13:39:26 +0100

    CHANGED: use useMember hook

--- src/views/orchestras/:id/_private-chat.js
+++ src/views/orchestras/:id/_private-chat.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useQuery } from "@apollo/react-hooks";
-import { memberByIdDocument } from "../../../config/documents";
+// import { useQuery } from "@apollo/react-hooks";
+import useMember from '../../../hooks/useMember';
+// import { memberByIdDocument } from "../../../config/documents";
 
 export default function PrivateChatView({ memberId }) {
   const params = useParams();
   const orchestraId = params.id;
 
-  const { data, loading, error } = useQuery(memberByIdDocument, {
-    variables: {
-      orchestraId,
-      memberId
-    }
-  });
+  const member = useMember(orchestraId, memberId);
 
   return (
     <div>
       <span>this is a private chat</span>
-      {data && <span> with {data.memberById.user.name}</span>}
-      {error && <span>{error.message}</span>}
-      {loading && <span>loading ...</span>}
+      {member && <span> with {member.user.name}</span>}
+      {/* {error && <span>{error.message}</span>}
+      {loading && <span>loading ...</span>} */}
     </div>
   );
 }
