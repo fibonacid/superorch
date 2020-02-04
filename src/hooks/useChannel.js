d4293d98e48329e5deedec54ef9d0709cc689551
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 12:30:24 +0100

    ADDED: hook to get data from a specific channel

---
+++ src/hooks/useChannel.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import {channelByIdDocument} from '../config/documents';
+import {useQuery} from '@apollo/react-hooks';
+
+export default function useChannel(orchestraId, channelId) {
+   const [channel, setChannel] = useState(null);
+   const { data } = useQuery(channelByIdDocument, {
+      variables: {
+         orchestraId,
+         channelId
+      }
+   });
+
+   useEffect(() => {
+      if(data) {
+         setChannel(
+            data.channelById
+         )
+      }
+   })
+
+   return channel;
+}
