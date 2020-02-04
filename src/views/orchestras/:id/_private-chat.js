0b2ffd109d7265a938a60b43e18784376d3ec5af
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 12:12:10 +0100

    ADDED: fetch member data

--- src/views/orchestras/:id/_private-chat.js
+++ src/views/orchestras/:id/_private-chat.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import { memberByIdDocument } from "../../../config/documents";
 
 export default function PrivateChatView({ memberId }) {
-   return <div>this is a private chat with {memberId}</div>
+  const params = useParams();
+  const orchestraId = params.id;
+
+  const { data, loading, error } = useQuery(memberByIdDocument, {
+    variables: {
+      orchestraId,
+      memberId
+    }
+  });
+
+  return (
+    <div>
+      <span>this is a private chat</span>
+      {data && <span> with {data.memberById.user.name}</span>}
+      {error && <span>{error.message}</span>}
+      {loading && <span>loading ...</span>}
+    </div>
+  );
 }
