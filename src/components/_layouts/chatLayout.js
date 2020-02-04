0fbe8bc9d830edf84b25f15db28825e13d0a9309
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 23:29:19 +0100

    ADDED: chat layout

---
+++ src/components/_layouts/chatLayout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+export const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Header = styled.h2`
+   padding: 10px;
+   background: whitesmoke;
+   border-bottom: solid 1px lightgrey;
+   font-size: 16px;
+`;
