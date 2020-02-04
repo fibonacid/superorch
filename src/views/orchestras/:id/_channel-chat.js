e50b2d62d01aa13eec81ff657472d697eae41d21
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 12:30:42 +0100

    ADDED: fetch channel messages

--- src/views/orchestras/:id/_channel-chat.js
+++ src/views/orchestras/:id/_channel-chat.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { channelByIdDocument } from '../../../config/documents';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/react-hooks";
+import useChannel from "../../../hooks/useChannel";
+import { channelMessagesDocument } from "../../../config/documents";
 
 export default function ChannelChatView({ channelId }) {
-   const params = useParams();
-   const orchestraId = params.id;
+  const params = useParams();
+  const orchestraId = params.id;
 
-   const { data, loading, error } = useQuery(channelByIdDocument, {
-      variables: {
-         orchestraId,
-         channelId
-      }
-   });
+  const channel = useChannel(orchestraId, channelId);
 
-   return (
-      <div>
-         {data && <h2> {data.channelById.name}</h2>}
-         {error && <span>{error.message}</span>}
-         {loading && <span>loading ...</span>}
-      </div>
-   )
+  const { data, loading, error } = useQuery(channelMessagesDocument, {
+    variables: {
+      orchestraId,
+      channelId
+    }
+  });
+
+  useEffect(() => {
+     console.log(data)
+  }, [data]);
+
+  return (
+    <div>
+      {channel && <h2> {channel.name}</h2>}
+      {data && (
+         data.channelMessages.map((message, index) => (
+            <div key={index}>
+               <span>{message.from.user.name} sent: </span>
+               <span>{message.body}</span>
+            </div>
+         ))
+      )}
+      {error && <span>{error.message}</span>}
+      {loading && <span>loading ...</span>}
+    </div>
+  );
 }
