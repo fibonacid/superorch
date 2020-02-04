a6c319ed2227f375a7ca487c51a186a1e9755cbf
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-04 15:49:40 +0100

    ADDED: form to submit new message

--- src/components/MessageBoard/InputBox/index.js
+++ src/components/MessageBoard/InputBox/index.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import styled from "styled-components/macro";
 
-const StyledContainer = styled.div`
-   display: flex;
+const StyledForm = styled.form`
+  display: flex;
+  padding: 10px;
 `;
 
 const StyledInput = styled.textarea`
-   flex: 1;
-   resize: none;
+  flex: 1;
+  resize: none;
+  border-radius: 10px;
+  border: solid 1px lightgrey;
+`;
+
+const StyledButton = styled.input`
+   margin-left: 5px;
+   border-radius: 10px;
 `;
 
 export default function InputBox({ onSend }) {
+  const [value, setValue] = useState("");
+
+  const onChange = useCallback(event => {
+    event.preventDefault();
+    setValue(event.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    event => {
+      event.stopPropagation();
+      onSend(value);
+      setValue("");
+    },
+    [value]
+  );
+
   return (
-    <StyledContainer>
-      <StyledInput></StyledInput>
-      <button>Send</button>
-    </StyledContainer>
+    <StyledForm onSubmit={onSubmit}>
+      <StyledInput value={value} onChange={onChange} />
+      <StyledButton type="submit" />
+    </StyledForm>
   );
 }
