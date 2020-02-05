316f687ecc5aff60b364daeda8e11d71e66c323b
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-05 17:45:45 +0100

    CHANGED: avoid infinite loops

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -1,31 +1,36 @@
-import { useState, useEffect } from "react";
+import { useCallback } from "react";
 import useChannel from "./useChannel";
 import useMember from "./useMember";
 import useChannelMessages from "./useChannelMessages";
 import usePrivateMessages from "./usePrivateMessages";
 
 export default function useChat(orchestra, type, id) {
-  const [title, setTitle] = useState("");
-  const [messages, setMessages] = useState([]);
-
   const channel = useChannel(orchestra, id);
-  const [channelMessages= []] = useChannelMessages(orchestra, id);
+  const [channelMessages] = useChannelMessages(orchestra, id);
 
   const member = useMember(orchestra, id);
-  const [privateMessages = []] = usePrivateMessages(orchestra, id);
+  const [privateMessages] = usePrivateMessages(orchestra, id);
 
-  useEffect(
-    function() {
-      if (type === "channel") {
-        channel && setTitle(channel.name);
-        channelMessages && setMessages(channelMessages);
-      } else if (type === "member") {
-        member && setTitle(member.user.name);
-        privateMessages && setMessages(privateMessages);
-      }
-    },
-    [channel, channelMessages, member, privateMessages]
-  );
+  const getTitle = useCallback(function() {
+    if (type === "channel") {
+      return channel?.name;
+    }
+    if (type === "member") {
+      return member?.user?.name;
+    }
+  }, [channel, member]);
 
-  return { title, messages };
+  const getMessages = useCallback(function() {
+    if (type === "channel") {
+      return channelMessages;
+    }
+    if (type === "member") {
+      return privateMessages;
+    }
+  }, [channelMessages, privateMessages]);
+
+  return {
+    title: getTitle() || "",
+    messages: getMessages() || []
+  };
 }
