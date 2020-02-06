432d7fabd63aa80ae364e8110aab0005caa2753b
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-06 21:04:36 +0100

    ADDED: playground component

--- src/views/orchestras/show/members/show.js
+++ src/views/orchestras/show/members/show.js
@@ -3,8 +3,9 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/react-hooks";
 import { getMember } from "../../../../api/members";
 import usePrivateMessages from "../../../../hooks/usePrivateMessages";
-import * as chatLayout from "../../../../components/_layouts/chatLayout";
+import * as ChatLayout from "../../../../components/_layouts/ChatLayout";
 import MessageBoard from "../../../../components/MessageBoard";
+import Playground from "../../../../components/Playground";
 
 export default function OrchestraMemberShowView() {
   const { orchestra: orchestraId, member: memberId } = useParams();
@@ -30,9 +31,12 @@ export default function OrchestraMemberShowView() {
   }, []);
 
   return (
-    <chatLayout.Container>
-      <chatLayout.Header>{data?.member?.user?.name || "empty"}</chatLayout.Header>
-      <MessageBoard messages={messages} onSend={onSend} />
-    </chatLayout.Container>
+    <ChatLayout.Wrapper>
+      <ChatLayout.Header>{data?.member?.user?.name || "Member"}</ChatLayout.Header>
+      <ChatLayout.Container>
+        <Playground />
+        <MessageBoard messages={messages} onSend={onSend} />
+      </ChatLayout.Container>
+    </ChatLayout.Wrapper>
   );
 }
