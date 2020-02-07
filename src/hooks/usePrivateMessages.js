1133642d5efc76c2f20d6bba0e7eab65ee25d654
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-07 13:49:47 +0100

    ADDED: newPrivateMessage subscription

--- src/hooks/usePrivateMessages.js
+++ src/hooks/usePrivateMessages.js
@@ -1,29 +1,54 @@
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { useEffect, useCallback } from "react";
 import {
-   GET_PRIVATE_MESSAGES_QUERY,
-   SEND_PRIVATE_MESSAGE_MUTATION
- } from "../api/messages";
- import { useQuery, useMutation } from "@apollo/react-hooks";
- 
- export default function usePrivateMessages(orchestraId, memberId) {
-   const variables = {
-     orchestraId,
-     memberId
-   };
- 
-   const { data } = useQuery(GET_PRIVATE_MESSAGES_QUERY, { variables });
- 
-   const [sendPrivateMessage] = useMutation(SEND_PRIVATE_MESSAGE_MUTATION, {
-     refetchQueries: [
-       {
-         query: GET_PRIVATE_MESSAGES_QUERY,
-         variables
-       }
-     ]
-   });
- 
-   return [
-      data ? data.privateMessages : [], 
-      sendPrivateMessage
-    ];
- }
- 
+  GET_PRIVATE_MESSAGES_QUERY,
+  SEND_PRIVATE_MESSAGE_MUTATION,
+  NEW_PRIVATE_MESSAGE_SUBSCRIPTION
+} from "../api/messages";
+
+export default function usePrivateMessages(
+  orchestraId,
+  memberId,
+  contexts = ["CHAT", "SUPERCOLLIDER"],
+  formats = ["PLAIN_TEXT", "JSON", "SC_RAW", "SC_LANG"]
+) {
+  const variables = {
+    orchestraId,
+    memberId,
+    filters: {
+      contexts,
+      formats
+    }
+  };
+
+  const { subscribeToMore, data } = useQuery(GET_PRIVATE_MESSAGES_QUERY, {
+    variables
+  });
+
+  const subscribe = useCallback(() =>
+    subscribeToMore({
+      document: NEW_PRIVATE_MESSAGE_SUBSCRIPTION,
+      variables,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData) return prev;
+        const { newPrivateMessage } = subscriptionData.data;
+        return {
+          privateMessage: [...prev.privateMessages, newPrivateMessage]
+        };
+      }
+    })
+  );
+
+  useEffect(subscribe, []);
+
+  const [sendPrivateMessage] = useMutation(SEND_PRIVATE_MESSAGE_MUTATION, {
+    refetchQueries: [
+      {
+        query: GET_PRIVATE_MESSAGES_QUERY,
+        variables
+      }
+    ]
+  });
+
+  return [data ? data.privateMessages : [], sendPrivateMessage];
+}
