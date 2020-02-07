8c62e689966446931b95f5e500b858cd0b7b56af
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-07 11:44:46 +0100

    ADDED: show username on channel messages

--- src/components/MessageBoard/MessageList/ListItem/index.js
+++ src/components/MessageBoard/MessageList/ListItem/index.js
@@ -15,13 +15,26 @@ const StyledInner = styled.div`
   font-size: 14px;
 `;
 
+const StyledUsername = styled.p`
+  color: blue;
+  font-size: 13px;
+  margin-bottom: 5px;
+`;
+
 export default function ListItem({ message }) {
   const [userId] = useState(localStorage.getItem("userId"))
   const isLoggedUser = message.from.user._id === userId;
 
+  const username = message?.from?.user?.name;
+  const isPrivate = message.__typename === "PrivateMessage" 
+  const showUsername = !isLoggedUser && !isPrivate;
+
   return (
     <StyledContainer right={isLoggedUser}>
-      <StyledInner>{message.body}</StyledInner>
+      <StyledInner>
+        {showUsername && <StyledUsername>{username}</StyledUsername>}
+        <div>{message.body}</div>
+      </StyledInner>
     </StyledContainer>
   );
 }
