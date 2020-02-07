e2eac3e10a673ee7fd8656847a32d223a42b74fa
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-07 12:20:51 +0100

    ADDED: subscribe to new channel messages

--- src/hooks/useChannelMessages.js
+++ src/hooks/useChannelMessages.js
@@ -1,16 +1,45 @@
+import { useEffect, useCallback } from "react";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import {
   GET_CHANNEL_MESSAGES_QUERY,
-  SEND_CHANNEL_MESSAGE_MUTATION
+  SEND_CHANNEL_MESSAGE_MUTATION,
+  NEW_CHANNEL_MESSAGE_SUBSCRIPTION
 } from "../api/messages";
-import { useQuery, useMutation } from "@apollo/react-hooks";
 
-export default function useChannelMessages(orchestraId, channelId) {
+export default function useChannelMessages(
+  orchestraId,
+  channelId,
+  contexts = ["CHAT", "SUPERCOLLIDER"],
+  formats = ["PLAIN_TEXT", "JSON", "SC_RAW", "SC_LANG"]
+) {
   const variables = {
     orchestraId,
-    channelId
+    channelId,
+    filters: {
+      contexts,
+      formats
+    }
   };
 
-  const { data } = useQuery(GET_CHANNEL_MESSAGES_QUERY, { variables });
+  const { subscribeToMore, data } = useQuery(GET_CHANNEL_MESSAGES_QUERY, {
+    variables
+  });
+
+  const subscribe = useCallback(() =>
+    subscribeToMore({
+      document: NEW_CHANNEL_MESSAGE_SUBSCRIPTION,
+      variables,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData) return prev;
+        const { newChannelMessage } = subscriptionData.data;
+        return {
+          channelMessages: [...prev.channelMessages, newChannelMessage]
+        };
+      }
+    })
+  );
+
+  useEffect(subscribe, []);
 
   const [sendChannelMessage] = useMutation(SEND_CHANNEL_MESSAGE_MUTATION, {
     refetchQueries: [
@@ -21,8 +50,5 @@ export default function useChannelMessages(orchestraId, channelId) {
     ]
   });
 
-  return [
-     data ? data.channelMessages : [], 
-     sendChannelMessage
-   ];
+  return [data ? data.channelMessages : [], sendChannelMessage];
 }
