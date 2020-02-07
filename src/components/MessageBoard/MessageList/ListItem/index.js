ff752bd0478f23681281ad6e1a78cfe527cfcfd7
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-07 15:11:20 +0100

    ADDED: speach bubble component

--- src/components/MessageBoard/MessageList/ListItem/index.js
+++ src/components/MessageBoard/MessageList/ListItem/index.js
@@ -1,17 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import SpeachBubble from "../../../_miscellaneous/SpeachBubble";
 
 const StyledContainer = styled.li`
-  margin: 5px;
+  margin: 5px 20px;
   display: flex;
   flex-direction: ${props => (props.right ? "row-reverse" : "row")};
-`;
-
-const StyledInner = styled.div`
-  display: inline;
-  background: white;
-  padding: 8px;
-  border-radius: 8px;
   font-size: 14px;
 `;
 
@@ -22,19 +16,19 @@ const StyledUsername = styled.p`
 `;
 
 export default function ListItem({ message }) {
-  const [userId] = useState(localStorage.getItem("userId"))
+  const [userId] = useState(localStorage.getItem("userId"));
   const isLoggedUser = message.from.user._id === userId;
 
   const username = message?.from?.user?.name;
-  const isPrivate = message.__typename === "PrivateMessage" 
+  const isPrivate = message.__typename === "PrivateMessage";
   const showUsername = !isLoggedUser && !isPrivate;
 
   return (
     <StyledContainer right={isLoggedUser}>
-      <StyledInner>
+      <SpeachBubble right={isLoggedUser}>
         {showUsername && <StyledUsername>{username}</StyledUsername>}
         <div>{message.body}</div>
-      </StyledInner>
+      </SpeachBubble>
     </StyledContainer>
   );
 }
---
+++ src/components/_miscellaneous/SpeachBubble/index.js
@@ -0,0 +1,37 @@
+import styled, {css} from 'styled-components/macro';
+
+const SpeachBubble = styled.div`
+  position: relative;
+  background: white;
+  border-radius: .4em;
+
+  padding: 8px;
+
+  &:after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    width: 0;
+    height: 0;
+    border: 0.813em solid transparent;
+    margin-top: -0.281em;
+    ${props =>
+      props.right
+        ? css`
+            right: 0;
+            border: 0.813em solid transparent;
+            border-left-color: white;
+            border-right: 0;
+            border-bottom: 0;
+            margin-right: -0.562em;
+          `
+        : css`
+            left: 0;
+            border-right-color: white;
+            border-left: 0;
+            border-bottom: 0;
+            margin-left: -0.562em;
+          `}
+`;
+
+export default SpeachBubble;
