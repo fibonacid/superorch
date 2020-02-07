53fd0de14e9596192a7131748878d3a3f93a522f
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-07 18:21:02 +0100

    ADDED: style for SC_RAW and SC_LANG messages

--- src/components/MessageBoard/Message/index.js
+++ src/components/MessageBoard/Message/index.js
@@ -1,52 +1,59 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import SpeachBubble from "../../_miscellaneous/SpeachBubble";
 
 const StyledContainer = styled.li`
   margin: 5px 20px;
   display: flex;
-  flex-direction: ${props => (props.right ? "row-reverse" : "row")};
+  flex-direction: ${props => props.direction === "right" && "row-reverse"};
   font-size: 14px;
-  overflow-wrap: break-word;
 `;
 
 const StyledUsername = styled.p`
   color: blue;
   font-size: 13px;
   margin-bottom: 5px;
+  font-weight: bold;
 `;
 
-const StyledSC = styled.p`
-   font-family: monospace;
-   color: grey;
-   font-size: 13px;
-   text-overflow: ellipsis;
+const StyledSCBody = styled.p`
+  font-family: monospace;
+  color: rgb(0,255,0);
+  font-size: 12px;
+  text-overflow: ellipsis;
+  max-width: 100%;
+  overflow: hidden;
+  white-space: nowrap;
 `;
 
 export default function ListItem({ message }) {
   const [userId] = useState(localStorage.getItem("userId"));
-  const isLoggedUser = message.from.user._id === userId;
+  const isMe = message.from.user._id === userId;
 
   const username = message?.from?.user?.name;
   const isPrivate = message.__typename === "PrivateMessage";
-  const showUsername = !isLoggedUser && !isPrivate;
+  const showName = !isMe && !isPrivate;
+
+  const direction = isMe ? "right" : "left";
+  const { format, body } = message;
+  const background = format === "SC_RAW" || format === "SC_LANG" ? "black" : "white";
 
   return (
-    <StyledContainer right={isLoggedUser}>
-      <SpeachBubble right={isLoggedUser}>
-        {showUsername && <StyledUsername>{username}</StyledUsername>}
-        {renderBody(message.format, message.body)}
+    <StyledContainer direction={direction}>
+      <SpeachBubble direction={direction} color={background}>
+        {showName && <StyledUsername>{username}</StyledUsername>}
+        {renderBody(format, body)}
       </SpeachBubble>
     </StyledContainer>
   );
 }
 
 function renderBody(format, body) {
-  switch(format) {
+  switch (format) {
     case "SC_RAW":
     case "SC_LANG":
-      return <StyledSC>{body}</StyledSC>
+      return <StyledSCBody>{body}</StyledSCBody>;
     default:
-      return <p>{body}</p>
+      return <p>{body}</p>;
   }
 }
