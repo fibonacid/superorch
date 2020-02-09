7f5e39c0ca8ac97a8a2cf023e3b4514809a59c45
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-09 21:26:28 +0100

    CHANGED: removed unnecessary component

--- src/components/MainNav/Selector/Item/index.js
+++ src/components/MainNav/Selector/Item/index.js
@@ -1,27 +1,50 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import Icon from "./Icon";
+import styled, {css} from "styled-components/macro";
+import { NavLink } from "react-router-dom";
+
+const activeStyle = {
+  background: "rgba(255,255,255,1)",
+  color: "black"
+}
+
+const StyledLink = styled(NavLink)`
+  width: 40px;
+  height: 40px;
+  margin: 0 auto;
+  display: block;
+  position: relative;
+  border: solid 1px white;
+  cursor: pointer;
+  border-radius: 8px;
+  
+  background: rgba(255,255,255,0);
+  color: white;
+  transition: background 0.2s ease-in;
+
+  &:hover { ${css(activeStyle)} }
+
+  &:not(:first-of-type) {
+    margin-top: 10px;
+  }
+`;
+
+const StyledLetter = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 function SelectorIcon(props) {
-  const history = useHistory();
-  const targetUrl = `/orchestras/${props.orchestra._id}`;
-  const match = history?.location?.pathname?.includes(targetUrl);
-
-  function handleClick() {
-    history.push(targetUrl);
-
-    props.onClick();
-  }
-
   const letter = props.orchestra.name.charAt(0).toUpperCase();
-
   return (
-    <Icon 
-      letter={letter}
-      onClick={handleClick} 
-      active={props.active || match} 
-    />
-  )
+    <StyledLink
+      to={`/orchestras/${props.orchestra._id}`}
+      activeStyle={activeStyle}
+    >
+      <StyledLetter>{letter}</StyledLetter>
+    </StyledLink>
+  );
 }
 
 export default SelectorIcon;
