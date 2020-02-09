f4a063120737d755c2ffc236066ee29871e128c5
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-09 19:02:32 +0100

    ADDED: highlight current link

--- src/components/OrchestraNav/ChannelList/index.js
+++ src/components/OrchestraNav/ChannelList/index.js
@@ -1,34 +1,38 @@
-import React from 'react';
-import styled from 'styled-components/macro';
-import {Link} from 'react-router-dom';
+import React from "react";
+import styled from "styled-components/macro";
+import { Link, useHistory } from "react-router-dom";
 
 const StyledList = styled.ul`
-   margin-top: 30px;
-   font-size: 15px;
+  margin-top: 30px;
+  font-size: 15px;
 `;
 
 const StyledItem = styled.li`
-   color: grey;
-   padding: 5px 0;
-   a {
-      color: black;
-      text-decoration: none;
-    }
+  color: grey;
+  padding: 5px 0;
+  a {
+    color: black;
+    text-decoration: none;
+    font-weight: ${props => (props.current ? "bold" : "normal")};
+  }
 `;
 
 export default function ChannelList({ orchestra }) {
+  const { location } = useHistory();
+  const base = `/orchestras/${orchestra._id}/chats`;
 
-   return (
-      <StyledList>
-         {
-            orchestra.channels.map((channel, index) => (
-               <StyledItem key={index}>
-                  <Link to={`/orchestras/${orchestra._id}/chats/channel-${channel._id}`}>
-                     # {channel.name}
-                  </Link>
-               </StyledItem>
-            ))
-         }
-      </StyledList>
-   )
+  return (
+    <StyledList>
+      {orchestra.channels.map((channel, index) => {
+        const slug = `channel-${channel._id}`;
+        const url = base + "/" + slug;
+        const match = location?.pathname?.includes(slug);
+        return (
+          <StyledItem key={index} current={match}>
+            <Link to={url}>{channel.name}</Link>
+          </StyledItem>
+        );
+      })}
+    </StyledList>
+  );
 }
--- src/components/OrchestraNav/MemberList/ListItem/index.js
+++ src/components/OrchestraNav/MemberList/ListItem/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
-import {Link} from 'react-router-dom';
+import { Link, useHistory } from "react-router-dom";
 
 const StyledContainer = styled.li`
   padding: 5px 0;
@@ -10,19 +10,23 @@ const StyledContainer = styled.li`
   a {
     color: black;
     text-decoration: none;
+    font-weight: ${props => (props.current ? "bold" : "normal")};
   }
 `;
 
 function ListItem({ orchestra, member }) {
+  const { location } = useHistory();
   const { user } = member;
 
+  const base = `/orchestras/${orchestra._id}/chats`;
+  const slug = `member-${member._id}`;
+  const url = base + "/" + slug;
+
+  const match = location?.pathname?.includes(slug);
+
   return (
-    <StyledContainer>
-      <Link
-        to={`/orchestras/${orchestra._id}/chats/member-${member._id}`}
-      >
-        {user.name}
-      </Link>
+    <StyledContainer current={match}>
+      <Link to={url}>{user.name}</Link>
     </StyledContainer>
   );
 }
