94443343f51f6b533314a3bff2101799b79af323
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-10 16:57:02 +0100

    CHANGED: keep a max of 4 messages

--- src/context/flash-message-context.js
+++ src/context/flash-message-context.js
@@ -6,42 +6,41 @@ export const FlashMessageContext = createContext({
   addMessage: (values, options) => {}
 });
 
+let counter = -1;
+
 export function FlashMessageProvider({ children }) {
   const [messages, setMessages] = useState([]);
 
   const createMessage = useCallback(
-    (value = "Oops, empty message", options = {}) => ({
-      type: "info",
-      disappear: true,
-      ...options,
-      id: messages.length,
-      value
-    }),
-    [messages]
+    (value = "Oops, empty message", options = {}) => {
+       counter++;
+       return {
+         type: "info",
+         disappear: true,
+         ...options,
+         value,
+         id: counter
+       }
+    },
+    [messages, counter]
   );
 
   const addMessage = useCallback(
-     function(value, options) {
-        const message = createMessage(value, options);
-        setMessages([
-           message,
-           ...messages,
-        ]);
-        return message;
-     },
-     [messages, setMessages, createMessage]
-  )
+    function(value, options) {
+      const message = createMessage(value, options);
+      setMessages([message, ...messages].splice(0, 4));
+      return message;
+    },
+    [messages, setMessages, createMessage, counter]
+  );
 
   const removeMessage = useCallback(
-     function(id) {
-        setMessages(
-           messages.filter(message => message.id !== id)
-        )
-        return id;
-     },
-     [messages, setMessages]
-  )
-
+    function(id) {
+      setMessages(messages.filter(message => message.id !== id));
+      return id;
+    },
+    [messages, setMessages]
+  );
 
   return (
     <FlashMessageContext.Provider
