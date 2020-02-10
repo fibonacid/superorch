f16aa1a9cc42bec83db294805e20227f7c5107cf
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-10 17:44:27 +0100

    ADDED: self destroy messages after two seconds

--- src/components/FlashMessages/Message/index.js
+++ src/components/FlashMessages/Message/index.js
@@ -1,7 +1,8 @@
-import React, {useCallback} from "react";
-import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import React, { useCallback } from "react";
+import styled from "styled-components";
+import useInterval from "../../../hooks/useInterval";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const StyledContainer = styled.li`
   background: rgba(255, 255, 0, 0.9);
@@ -14,21 +15,24 @@ const StyledContainer = styled.li`
 `;
 
 const StyledBody = styled.div`
-   flex: 1;
+  flex: 1;
 `;
 
 const StyledIcon = styled(FontAwesomeIcon)`
-   margin-left: 5px;
+  margin-left: 5px;
 `;
 
 export default function Message({ message, onRemove }) {
-  const onClick = useCallback(onRemove.bind(null, message.id), [
+  const destroy = useCallback(onRemove.bind(null, message.id), [
     onRemove,
     message
   ]);
 
+  // Self-destuct after one second
+  useInterval(destroy, 2000);
+
   return (
-    <StyledContainer onClick={onClick}>
+    <StyledContainer onClick={destroy}>
       <StyledBody>{message.value}</StyledBody>
       <StyledIcon icon={faTimes} />
     </StyledContainer>
