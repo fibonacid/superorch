d09c65f267fa75aa58a2b5e6f1efd76afd0e632b
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-10 13:17:56 +0100

    ADDED: copy text to clipboard

--- src/components/MessageBoard/Message/SCLang/index.js
+++ src/components/MessageBoard/Message/SCLang/index.js
@@ -1,11 +1,16 @@
-import React from "react";
-import styled from 'styled-components/macro';
+import React, { useCallback } from "react";
+import styled from "styled-components/macro";
 import SpeachBubble from "../../../_miscellaneous/SpeachBubble";
-import Header from '../Header';
+import Header from "../Header";
+import { copyToClipboard } from "../../../../helpers/electron";
+
+const StyledContainer = styled(SpeachBubble)`
+  cursor: pointer;
+`;
 
 const StyledBody = styled.p`
   font-family: monospace;
-  color: rgb(0,255,0);
+  color: rgb(0, 255, 0);
   font-size: 12px;
   text-overflow: ellipsis;
   max-width: 100%;
@@ -13,16 +18,28 @@ const StyledBody = styled.p`
   white-space: nowrap;
 `;
 
-export default function PlainText({
+export default function SCLang({
   direction = "right",
-  children,
+  body,
   username,
   showUsername = false
 }) {
+
+  // Copy text to clipboard
+  // Note: this will only work on electron.
+  const onClick = useCallback(() => {
+    const { clipboard } = window.require("electron");
+    clipboard.writeText(body);
+
+    if(clipboard.readText() === body) {
+      alert('Copied to clipboard')
+    }
+  }, [body]);
+
   return (
-    <SpeachBubble direction={direction} color="black">
+    <StyledContainer onClick={onClick} direction={direction} color="black">
       <Header username={username} showUsername={showUsername} />
-      <StyledBody>{children}</StyledBody>
-    </SpeachBubble>
+      <StyledBody>{body}</StyledBody>
+    </StyledContainer>
   );
 }
