878306cdd7b4a0268dcbb3929fa440ef0e3a2ecc
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-10 16:56:14 +0100

    CHANGED: flex-direction

--- src/components/FlashMessages/index.js
+++ src/components/FlashMessages/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import styled from "styled-components/macro";
 import useFlashMessages from "../../hooks/useFlashMessages";
-import Message from './Message';
+import Message from "./Message";
 
 const StyledList = styled.ul`
   bottom: 10px;
   left: 50px;
-  right: 50px; 
+  right: 50px;
   position: absolute;
   display: flex;
-  flex-direction: column;
+  flex-direction: column-reverse;
 `;
 
 export default function FlashMessages() {
@@ -18,7 +18,7 @@ export default function FlashMessages() {
   return (
     <StyledList>
       {messages.map((message, index) => (
-        <Message key={index} message={message} onRemove={removeMessage}/>
+        <Message key={index} message={message} onRemove={removeMessage} />
       ))}
     </StyledList>
   );
