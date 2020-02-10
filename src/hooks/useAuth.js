5b44c23adb110d6e2fd234427208c202b75f6a19
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-10 15:25:05 +0100

    CHANGED: rephrase error message

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -4,7 +4,7 @@ import { AuthContext } from "../context/auth-context";
 function useAuth() {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error("useBreakpoint must be used within BreakpointProvider");
+    throw new Error("useAuth must be used within AuthProvider");
   }
   return context;
 }
