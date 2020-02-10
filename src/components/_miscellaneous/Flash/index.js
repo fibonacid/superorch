721740cdbe1abc92ca6751420432a0e79e98a2bc
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-10 15:06:49 +0100

    ADDED: fade out flash message

--- src/components/_miscellaneous/Flash/index.js
+++ src/components/_miscellaneous/Flash/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import styled, {keyframes} from 'styled-components/macro';
 
 const appear = keyframes`
-   0% { opacity: 0.0 }
-   100% { opacity: 0.8 }
+   0%   { opacity: 0.0; }
+   10%  { opacity: 0.8; }
+   50%  { opacity: 0.8; }
+   100%  { opacity: 0.0; }
 `;
 
 const StyledContainer = styled.div`
@@ -14,7 +16,7 @@ const StyledContainer = styled.div`
    background: rgb(255, 255, 0);
    color: black;
    padding: 10px;
-   animation: ${appear} 0.5s;
+   animation: ${appear} 1s forwards;
 `;
 
 export default function Flash({ children, disappear = true}) {
