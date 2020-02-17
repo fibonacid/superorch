192ad42b7296feb49e8ae7e46240f886e719f6d3
Lorenzo Rivosecchi <lorenzorivosecchi3@gmail.com>
2020-02-17 16:49:48 +0100

    CHANGED: use env variables

--- app/fetch-schema.js
+++ app/fetch-schema.js
@@ -1,13 +1,21 @@
-const fetch = require('node-fetch');
-const fs = require('fs');
-const path = require('path');
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
 
-const API_HOST = process.env.API_HOST || 'http://localhost:5000';
-console.log(`Fetching ${API_HOST}/graphql ...`);
+require("dotenv").config();
 
-fetch(`${API_HOST}/graphql`, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
+const API_ENPOINT = process.env["REACT_APP_API_ENDPOINT"];
+if (!API_ENPOINT) {
+  throw new Error(
+    "Api endpoint is not set. Provide a value for env variable REACT_APP_API_ENDPOINT"
+  );
+}
+
+console.log(`... Fetching ${API_ENPOINT}`);
+
+fetch(API_ENPOINT, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
     variables: {},
     query: `
@@ -22,23 +30,29 @@ fetch(`${API_HOST}/graphql`, {
           }
         }
       }
-    `,
-  }),
+    `
+  })
 })
   .then(result => result.json())
   .then(result => {
     // here we're filtering out any type information unrelated to unions or interfaces
     const filteredData = result.data.__schema.types.filter(
-      type => type.possibleTypes !== null,
+      type => type.possibleTypes !== null
     );
     result.data.__schema.types = filteredData;
 
+    const filepath = path.join(__dirname, "./src/config/fragmentTypes.json");
+    const data = JSON.stringify(result.data, 0, 2);
 
-    fs.writeFile(path.join(__dirname, './src/config/fragmentTypes.json'), JSON.stringify(result.data), err => {
-      if (err) {
-        console.error('Error writing fragmentTypes file', err);
-      } else {
-        console.log('Fragment types successfully extracted!');
+    fs.writeFile(
+      filepath,
+      data,
+      err => {
+        if (err) {
+          console.error("Error writing fragmentTypes file", err);
+        } else {
+          console.log(`\n${data}\n\nFragment types successfully extracted.\n${filepath}\n`);
+        }
       }
-    });
+    );
   });
